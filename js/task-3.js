a9deb18511982c03fb8754bd70a35cc381a86d35
Vadim13245 <159133092+Vadim13245@users.noreply.github.com>
2024-03-01 14:07:00 +0300

    3456789

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function filterArray(numbers, value) {
+    const newNumbers = []
+    for(let i = 0; i <= numbers.length; i = ++) {
+        if(numbers[i] > value) {
+            newNumbers.push(numbers[i])
+        }
+    }
+
+    return newNumbwers
+}
